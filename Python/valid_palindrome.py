399ce4d97d6518df7e8655a50fb0400afd761557
Santhosh S <37366893+SanthoshS20@users.noreply.github.com>
2021-09-24 18:02:35 +0530

    Update valid_palindrome.py

--- Python/valid_palindrome.py
+++ Python/valid_palindrome.py
@@ -1,20 +1,20 @@
-def isPalindrome(self, s: str) -> bool:
+def isPalindrome(s: str) -> bool:
         string = s.lower()
         i = 0
-        j = len(s)-1
-        flag=0
-        while(i<j):
-            if((ord(string[i])>=97 and ord(string[i])<=122) or (ord(string[i])>=48 and ord(string[i])<=57)):
-                if((ord(string[j])>=97 and ord(string[j])<=122) or (ord(string[j])>=48 and ord(string[j])<=57)):
-                    if(ord(string[i])!=ord(string[j])):
-                        flag=1
+        j = len(s) - 1
+        flag = 0
+        while i < j:
+            if((ord(string[i]) >= 97 and ord(string[i]) <= 122) or (ord(string[i]) >= 48 and ord(string[i]) <= 57)):
+                if((ord(string[j]) >= 97 and ord(string[j]) <= 122) or (ord(string[j]) >= 48 and ord(string[j]) <= 57)):
+                    if ord(string[i]) != ord(string[j]):
+                        flag = 1
                         break
-                    i+=1
-                    j-=1
+                    i += 1
+                    j -= 1
                 else:
-                    j-=1
+                    j -= 1
             else:
-                i+=1
-        if(flag==1):
+                i += 1
+        if flag == 1:
             return False
         return True
