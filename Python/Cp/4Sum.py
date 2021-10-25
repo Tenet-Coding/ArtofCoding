482113224489f902bac4cb0849bc558a06c2ee81
sahilsingh7735 <sahilsingh7735@gmail.com>
2021-10-25 19:40:13 +0530

    First commit

---
+++ Python/Cp/4Sum.py
@@ -0,0 +1,50 @@
+"""
+Here: https://leetcode.com/problems/4sum/
+
+Given an array nums of n integers, return an array of all the unique quadruplets [nums[a], nums[b], nums[c], nums[d]] such that:
+
+0 <= a, b, c, d < n
+a, b, c, and d are distinct.
+nums[a] + nums[b] + nums[c] + nums[d] == target
+You may return the answer in any order.
+"""
+
+
+def fourSum(nums, target):
+    ans, n = [], len(nums)
+    nums.sort()
+    for a in range(n):
+        for b in range(a+1, n):
+            c = b+1
+            d = n-1
+            while c < d:
+                sums = nums[a]+nums[b]+nums[c]+nums[d]
+                if sums < target:
+                    c += 1
+                elif sums > target:
+                    d -= 1
+                else:
+                    to_append = [nums[a], nums[b], nums[c], nums[d]]
+                    if to_append not in ans:
+                        ans.append(to_append)
+                    c += 1
+                    d -= 1
+    return ans
+
+
+if __name__ == "__main__":
+    # taking the input
+    target = int(input("target: "))
+    nums = list(map(int, input("Enter the array ").split()))
+    print(fourSum(nums, target))
+
+"""
+
+
+Input: nums = [1,0,-1,0,-2,2], target = 0
+Output: [[-2,-1,1,2],[-2,0,0,2],[-1,0,0,1]]
+
+Input: nums = [2,2,2,2,2], target = 8
+Output: [[2,2,2,2]]
+
+"""
--- Python/Cp/climbing-the-leaderboard.py
+++ Python/Cp/climbing-the-leaderboard.py
@@ -2,7 +2,7 @@
 here : https://www.hackerrank.com/challenges/climbing-the-leaderboard/problem
 
 An arcade game player wants to climb to the top of the leaderboard and track their ranking.
- The game uses Dense Ranking, so its leaderboard works like this:
+The game uses Dense Ranking, so its leaderboard works like this:
 
 The player with the highest score is ranked number 1 on the leaderboard.
 Players who have equal scores receive the same ranking number, and the next player(s) receive the immediately following ranking number.
